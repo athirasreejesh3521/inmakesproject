918528b076fc1230e17686774022a5dbe7aff137
Athira Rajan <athirarajan8383@gmail.com>
2023-04-04 21:25:30 +0530

    added

--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render,redirect
 from . models import Task
 from . forms import TodoForm
-from django.views.generic import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView,DeleteView
-from django.urls import reverse_lazy
 
 # Create your views here.
 def home(request):
@@ -16,33 +12,6 @@ def home(request):
         task = Task(name=name, priority=priority, date=date)
         task.save()
     return render(request, "home.html",{'detail':detail})
-
-class TaskListView(ListView):
-    model = Task
-    template_name = 'home.html'
-    context_object_name = 'detail'
-
-
-class TaskDetailView(DetailView):
-    model = Task
-    template_name = 'detail.html'
-    context_object_name = 'task'
-
-
-class TaskUpdateView(UpdateView):
-    model = Task
-    template_name = 'update.html'
-    context_object_name = 'task'
-    fields = ['name', 'priority', 'date']
-
-    def get_success_url(self):
-        return reverse_lazy('cbvdetail',kwargs={'pk':self.object.id})
-
-
-class TaskDeleteView(DeleteView):
-    model = Task
-    template_name = 'delete.html'
-    success_url=reverse_lazy('cbvhome')
 
 def delete(request,taskid):
     task=Task.objects.get(id=taskid)
